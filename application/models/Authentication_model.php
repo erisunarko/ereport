d6ba5209e4ddaa2381d939d5797f1c074d70cf99
erisunarko <erisunarko@gmail.com>
2018-10-21 21:44:41 +0700

    initial repo

---
+++ application/models/Authentication_model.php
@@ -0,0 +1,35 @@
+<?php
+	
+	class Authentication_model extends CI_Model 
+	{
+		public function check_user($employeeId, $employeePass) 
+		{
+			$query	 		= $this->db->get_where('tb_users', array('employee_id' => $employeeId));;			
+			if ($query->num_rows() > 0) 
+			{
+				foreach ($query->result_array() as $dataRow) {
+					$result['employee_id']		= $dataRow['employee_id'];
+					$result['employee_password']= $dataRow['employee_password'];
+					$result['position']			= $dataRow['employee_position'];
+				
+					if (password_verify($employeePass, $result['employee_password']))
+					{
+						$result['auth']			= true;		
+						return $result;
+					}
+					else
+					{
+						$result['auth'] 		= false;
+						return $result;
+					}
+				}
+			} 
+			else 
+			{
+				$result['auth']					= 'no_res';
+				return $result;
+			}			
+		}
+	}
+
+?>
